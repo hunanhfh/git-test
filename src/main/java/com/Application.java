6b595d3ae534435c4dfc4b0cebb35468401b1bcd
hunanhfh <spring>
2019-04-19 10:58:23 +0800

    初始化项目

---
+++ src/main/java/com/Application.java
@@ -0,0 +1,10 @@
+package com;
+
+/**
+ * Created by hyd on 2019/4/19.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("hello word");
+    }
+}
