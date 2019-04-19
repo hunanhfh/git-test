3d48cea85704cf1e510c53c967e4f2a4246db904
hunanhfh <spring>
2019-04-19 11:01:35 +0800

    更新文件

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -6,5 +6,6 @@ package com;
 public class Application {
     public static void main(String[] args) {
         System.out.println("hello word");
+        System.out.println("hello word2");
     }
 }
